3d6203989e3729b8a96145c40bfc4bd9a8b3374d
Expertz <xeniapliner@gmail.com>
2023-06-29 18:35:39 +0300

    1 Seminar HW 2Task_2

--- Seminar 1/HW_02Task_2/Program.cs
+++ Seminar 1/HW_02Task_2/Program.cs
@@ -1,19 +1,16 @@
 ﻿//Напишите программу, которая принимает на вход три числа и 
 //выдаёт максимальное из этих чисел.
-int numA, numB, numC, Max;
 
 Console.WriteLine("Please put 3 numbers");
-
 int numA = int.Parse(Console.ReadLine());
 int numB = int.Parse(Console.ReadLine());
 int numC = int.Parse(Console.ReadLine());
-Max = numA;
+int Max = numA;
 
-if (numB > Max){
+if (numB > Max) {
     Max = numB;
 }
-else (numC > Max){
+if (numC > Max) {
     Max = numC;
 }
-
 Console.WriteLine($"Maximum number is {Max}");
