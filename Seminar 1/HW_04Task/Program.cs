65129a7ff09a924f3ca4b556ea86a0438db545f6
Expertz <xeniapliner@gmail.com>
2023-06-29 19:37:04 +0300

    1 Seminar HW_all done

---
+++ Seminar 1/HW_04Task/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Please put a number");
+int numA = int.Parse(Console.ReadLine());
+int i = 1;
+
+for (i = 1; i <= numA; i++){
+
+    if (i % 2 == 0) {
+Console.Write(i + "  ");
+}
+}
