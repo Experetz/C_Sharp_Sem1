9b28279fed214a7cfa1d9e020e42bfb0c9aa49ca
Expertz <xeniapliner@gmail.com>
2023-06-29 19:23:17 +0300

    1 Seminar HW 3Task

---
+++ Seminar 1/HW_03Task/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Please put a number");
+int numA = int.Parse(Console.ReadLine());
+
+int numB = numA % 2;
+Console.WriteLine(numB);
+
+if (numB == 0){
+   Console.WriteLine("Yes, your number is even");
+}
+else 
+Console.WriteLine("No, your number is not even");
+
